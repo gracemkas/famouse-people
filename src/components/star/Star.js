d15abe6ad5c2802434e5ebc8dd411936ad8c5b72
Grace Kasahara <gracekasahara@gmail.com>
2018-07-25 10:12:21 -0500

    added form, and curry

--- src/components/star/Star.js
+++ src/components/star/Star.js
@@ -7,33 +7,44 @@ class Star extends Component {
         this.state = {
             star: {           
                 name: '',
-                role: ''
+                role: '',
+                popularity: ''
             }
         }
     }
 
-    handleRole = (event) => {
-        console.log(event.target.value);
-        this.setState({
-            star: {
-                ...this.state.star,
-                role : event.target.value
-            }
-        })    
+    // handleRole = (event) => {
+    //     console.log(event.target.value);
+    //     this.setState({
+    //         star: {
+    //             ...this.state.star,
+    //             role : event.target.value
+    //         }
+    //     })    
+    // }
+
+    // handleName = (event) => {
+    //     console.log(event.target.value);
+    //     this.setState({
+    //         star : {
+    //             ...this.state.star,
+    //             name : event.target.value
+    //         }
+    //     })    
+    // }
+
+    handleChangeFor = (propertyName) => (event) => {
+            this.setState({
+                star: {
+                    ...this.state.star,
+                    [propertyName] : event.target.value
+                }
+            })
     }
 
-    handleName = (event) => {
-        console.log(event.target.value);
-        this.setState({
-            star : {
-                ...this.state.star,
-                name : event.target.value
-            }
-        })    
-    }
-
-    handleClick = () => {
-        console.log(this.state); 
+    handleClick = (event) => {
+        event.preventDefault();
+        console.log(this.state.star); 
     }
     
     
@@ -41,10 +52,13 @@ class Star extends Component {
     render() {
         return (
             <div>
-            <p> {this.state.star.name} is famous for {this.state.star.role} </p>
-            <input type="text" onChange={this.handleName}/>
-            <input type="text" onChange={this.handleRole}/>
-            <button onClick={this.handleClick}>Submit</button>
+            <form onSubmit={this.handleClick}>
+            <p> {this.state.star.name} is famous for {this.state.star.role}. Their popularity rating is {this.state.star.popularity} </p>
+            <input value={this.state.star.name} type="text" onChange={this.handleChangeFor('name')}/>
+            <input value={this.state.star.role} type="text" onChange={this.handleChangeFor('role')}/>
+            <input value={this.state.star.popularity} type="number" onChange={this.handleChangeFor('popularity')}/>
+            <input type="submit" value="Click me"/>
+            </form>
             </div>
         )
     }
